e624303990c64bf52ac424def31ddb4f07b31287
Jonas Rafael de Lima <jonasrafael16@gmail.com>
2023-11-02 20:47:48 -0300

    Feat: insert function to done task button

--- scripts-crud.js
+++ scripts-crud.js
@@ -7,8 +7,13 @@ const textArea = document.querySelector('.app__form-textarea');
 const cancelFormButton = document.querySelector('.app__form-footer__button--cancel');
 const deleteFormButton = document.querySelector('.app__form-footer__button--delete');
 const localStorageTasks = localStorage.getItem('tasks');
+const taskActiveDescription = document.querySelector('.app__section-active-task-description');
 
 let tasks = localStorageTasks ? JSON.parse(localStorageTasks) : [];
+let selectedTask = null;
+let itemTaskSelected = null;
+let taskInEdition = null;
+let textInEdition = null;
 
 
 const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -18,6 +23,26 @@ const taskIconSvg = `<svg class="app__section-task-icon-status" width="24" heigh
         d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
         fill="#01080E" />
 </svg>`;
+
+const selectTask = (task, item) => {
+  if (selectedTask) {
+    selectedTask.classList.remove('app__section-task-list-item--selected');
+  }
+
+  selectedTask = item;
+  selectedTask.classList.add('app__section-task-list-item--selected');
+  taskActiveDescription.textContent = task.description;
+  itemTaskSelected = task;
+}
+
+const selectTaskToEdit = () => {
+  formLabel.textContent = 'Editando tarefa';
+  formTask.classList.remove('hidden');
+  textArea.value = itemTaskSelected.description;
+  taskInEdition = itemTaskSelected;
+  textInEdition = textArea.value;
+}
+
 
 function createTask(task) {
   const li = document.createElement('li');
@@ -30,8 +55,27 @@ function createTask(task) {
   paragraph.classList.add('app__section-task-list-item-description');
   paragraph.textContent = task.description;
 
+  const button = document.createElement('button');
+
+
+  li.onclick = () => {
+    selectTask(task, li);
+  }
+
+  svgIcon.addEventListener('click', (event) => {
+    event.stopPropagation();
+    li.classList.add('app__section-task-list-item-complete');
+  });
+
+  if (task.done) {
+    button.setAttribute('disabled', true);
+    li.classList.add('app__section-task-list-item-complete');
+  }
+
+
   li.appendChild(svgIcon);
   li.appendChild(paragraph);
+  li.appendChild(button);
 
   return li;
 }
